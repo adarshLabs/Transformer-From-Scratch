bf088fa95f03c3bbcb924c0105876960734baf88
Adarsh Tiwari <tiwariadarshformal@gmail.com>
2026-05-25 18:12:32 +0530

    feedforward network completed

--- transformer_blocks/feedforward_networks.py
+++ transformer_blocks/feedforward_networks.py
@@ -2,5 +2,15 @@ import torch
 import torch.nn as nn
 
 class FeedForwardNetwork(nn.Module):
-    def __init(self):
-        pass
+    def __init(self, embed_dim, expansion_factor = 4, dropout=0.1):
+        super().__init__()
+        hidden_dim = expansion_factor* embed_dim
+        self.ffn = nn.Sequential(
+            nn.Linear(embed_dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, embed_dim)
+        )
+
+    def forward(self, x):
+        return self.ffn(x)
