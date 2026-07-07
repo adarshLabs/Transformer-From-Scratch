cda38b0d6c79cf73cdc3b5c5f52312b979b8fa7b
Adarsh Tiwari <tiwariadarshformal@gmail.com>
2026-07-08 02:06:19 +0530

    feat: Implement GPT decoder block and add initial GPT-2 model configuration

---
+++ transformer_models/gpt_decoder_block.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+from attention.multi_head_attention import MultiHeadAttention
+from positional_encoding.rotary_positional_embedding import RotaryPositionalEmbedding
+from transformer_blocks.feed_forward_network import FeedForwardNetwork
+
+class GPTDecoderBlock(nn.Module):
+    def __init__(self, embed_dim, num_heads, expansion_factor=4, dropout=0.1):
+
+        super().__init__()
+        rope = RotaryPositionalEmbedding(num_heads)
+        self.self_attention = MultiHeadAttention(embed_dim, num_heads=num_heads, dropout=dropout, qk_positional_encoding=rope)
+        self.ffn = FeedForwardNetwork(embed_dim=embed_dim, expansion_factor=4, dropout=dropout)
+        self.norm1 = nn.LayerNorm(embed_dim)
+        self.norm2 = nn.LayerNorm(embed_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, mask=None):
+        residual = x
+        x = self.norm1(x)
+        attn_out, _ = self.self_attention(x)
+        x = residual + self.dropout(attn_out)
+
+        residual = x
+        x = self.norm2(x)
+        x = residual + self.dropout(self.ffn(x))
+
+        return x
+
+
+
