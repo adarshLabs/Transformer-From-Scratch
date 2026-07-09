3701f969608caf63b207824186cbe386ae5d1f46
Adarsh Tiwari <tiwariadarshformal@gmail.com>
2026-07-09 23:59:59 +0530

    commit

--- transformer_models/gpt2.py
+++ transformer_models/gpt2.py
@@ -5,12 +5,15 @@ config = {
     "n_heads":6,
     "d_model":384,
     "vocab_size":50257,
-    "block_size":256
+    "block_size":256,
+    "expansion_factor":4,
+    "dropout":0.1,
+    "padding_token":0
 }
 
 class GPT2(nn.Module):
     def __init__(self):
-        pass
+        super().__init__()
+        self.token_embedding = nn.Embedding(config["vocab_size"], config["d_model"])
 
     
-
