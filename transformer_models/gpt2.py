af2ff4a86a19d9ecfffd19afd723eaff8c109024
Adarsh Tiwari <tiwariadarshformal@gmail.com>
2026-07-12 23:48:45 +0530

    feat: Enhance masking functions and implement mask building in GPT2 model

--- transformer_models/gpt2.py
+++ transformer_models/gpt2.py
@@ -1,10 +1,13 @@
+import torch
 import torch.nn as nn
 from dataclasses import dataclass
-
 try:
     from .gpt_decoder_block import GPTDecoderBlock
 except ImportError:
     from gpt_decoder_block import GPTDecoderBlock
+from attention.masking import causal_mask, padding_mask, combined_mask
+import torch.nn.functional as F
+
 
 @dataclass
 class GPT2Config:
@@ -48,5 +51,29 @@ class GPT2(nn.Module):
             nn.init.normal_(module.weight, mean=0, std=0.02)
 
     def _build_mask(self, input_ids):
-        pass
+        B, S = input_ids.shape
+
+        causal = causal_mask(S)
+        padding = padding_mask(input_ids, padding_token=self.config.padding_token)
+        return combined_mask(causal, padding)
     
+    def forward(self, input_ids, target=None):
+
+        embedding = self.token_embedding(input_ids)
+        x = self.drop(embedding)
+        mask = self._build_mask(input_ids)
+
+        for block in self.blocks:
+            x = block(x, mask=mask)
+
+        x = self.final_norm(x)
+        logits = self.lm_head(x)
+        loss = None
+        if target is not None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), target.view(-1))
+
+        return logits, loss
+
+    @torch.no_grad()
+    def generate(self, input_ids, max_new_tokens, temperature=1, top_k=50):
+        pass
